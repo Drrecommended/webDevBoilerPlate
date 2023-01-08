294447e0436bad73fe3c4ee6851aef03bbfd0d15
Tyler French <tylerfrench89@gmail.com>
2023-01-08 15:41:52 -0800

    finished prettier eslint configd

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import generateJoke from './generateJoke';
-import './styles/main.scss';
-import pillBottle from './assets/pillBottle.jpeg';
+import generateJoke from './generateJoke'
+import './styles/main.scss'
+import pillBottle from './assets/pillBottle.jpeg'
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('div')
 
-  element.innerText = generateJoke();
-  return element;
+  element.innerText = generateJoke()
+  return element
 }
 
-console.log(1, 2, 3, pillBottle);
-const myImg = new Image();
-myImg.src = pillBottle;
-myImg.classList.add('ima');
+console.log(1, 2, 3, pillBottle)
+const myImg = new Image()
+myImg.src = pillBottle
+myImg.classList.add('ima')
 
-document.body.appendChild(myImg);
+document.body.appendChild(myImg)
 
-document.body.appendChild(component());
+document.body.appendChild(component())
